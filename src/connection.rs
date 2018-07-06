877fc1c08a06dffed104e0f47bd3bd2b11ac4989
Vincent Foley <vfoley@gmail.com>
2018-07-06 11:36:45 -0400

    Make get_response_length() accept a &str
    Instead of doing a full string copy, pass a shared borrow of the
    response string to `connection::get_response_length()`.

--- src/connection.rs
+++ src/connection.rs
@@ -109,7 +109,7 @@ fn read_from_stream<T: Read>(stream: T) -> Result<String, Error> {
         if c == '\n' {
             // End of line, try to get the response length
             if blank_line && response_length.is_none() {
-                response_length = Some(get_response_length(response.clone()));
+                response_length = Some(get_response_length(&response));
             }
             blank_line = true;
         } else if c != '\r' {
@@ -131,7 +131,7 @@ fn read_from_stream<T: Read>(stream: T) -> Result<String, Error> {
 
 /// Tries to find out how long the whole response will eventually be,
 /// in bytes.
-fn get_response_length(response: String) -> usize {
+fn get_response_length(response: &str) -> usize {
     // The length of the headers
     let mut byte_count = 0;
     for line in response.lines() {
